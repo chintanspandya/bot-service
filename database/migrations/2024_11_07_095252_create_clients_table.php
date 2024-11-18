a24f6d5a6ae9e7146a823570a9fc1afc19a32a75
simab aspireedge <simab.koreja@apsireedge.com>
2024-11-18 12:47:39 +0530

    initial commit

---
+++ database/migrations/2024_11_07_095252_create_clients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('contact_no')->nullable();
+            $table->string('whatsapp_no')->nullable();
+            $table->decimal('paid_amount', 16, 2)->nullable();
+            $table->decimal('remaining_amount', 16, 2)->nullable();
+            $table->string('reminder_frequency')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
